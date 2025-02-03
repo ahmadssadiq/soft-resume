0461792ce5709705f566f604b8eeee67040ccfe9
ahmadssadiq <91856553+ahmadssadiq@users.noreply.github.com>
2025-02-03 18:07:19 -0500

    completed and fixed issue

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,9 +8,11 @@ import QZ from '../images/QuizIt.png';
 
 const projectData = [
   {
-    title: 'QuizIt - Quiz Application',
-    description: `Worked with American Civil Liberties Union (ACLU) and analyzed 8+ years of BPD overtime data using Python and pandas, uncovering a 260,000-hour annual discrepancy between worked and paid overtime hours.
-    Visualized data with matplotlib/seaborn, showing a 75% overlap between overtime and misconduct.`,
+    title: 'QuizIt - Interactive Quiz Application',
+    description: `Developed a Django-based web application enabling users to create, edit, and take quizzes dynamically. 
+    Implemented user authentication for secure access, a real-time quiz-taking experience, and automatic scoring. 
+    Built with Django, HTML, CSS, and JavaScript, the platform supports customizable quizzes with multiple-choice questions 
+    and performance tracking.`,
     image: QZ, // assuming you moved this image to the public folder
     link: '#/projects/Quiz-It',  // internal link
   },
